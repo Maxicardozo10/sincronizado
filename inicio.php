99a9936a1a38cfe1ef57db986ef5bd20a61dd713
maxi cardozo <maxialejandrocardozo@gmail.com>
2022-10-01 14:00:32 -0300

    segundocommit

---
+++ inicio.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" contet="width=device-width,initial-scale=1.0">
+        <meta htpp-equiv="x-UA-Compatible"
+        content="ie=edge">
+        <title>document</title>
+</head>
+<body>
+    
+</body>
+</html>
