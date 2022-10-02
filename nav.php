a3409cb2b1fec536896c1965be6e1b3d9acb5d72
Maxicardozo10 <114826781+Maxicardozo10@users.noreply.github.com>
2022-10-02 16:50:20 -0300

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ol>
+        <li><a href="#">home</a></li>
+        <li><a href="#">quienes somos</a></li>
+        <li><a href="#">nosotros</a></li>
+        <li><a href="#">contacto</a></li>
+</ol>
+</nav>
